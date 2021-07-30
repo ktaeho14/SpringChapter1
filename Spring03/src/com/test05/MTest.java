105132c3aab5e5414e90152bef049c8bbee186cd
ktaeho14 <ktaeho14@naver.com>
2021-07-30 18:01:56 +0900

    수정

--- Spring03/src/com/test05/MTest.java
+++ Spring03/src/com/test05/MTest.java
@@ -1,7 +1,17 @@
 package com.test05;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MTest {
 	public static void main(String[] args) {
+		ApplicationContext factory = new
+				ClassPathXmlApplicationContext("com/test05/applicationContext.xml");
 		
+		Developer kim = (Developer)factory.getBean("leess");
+		Engineer hong = (Engineer)factory.getBean("honggd");
+				
+		System.out.println(kim);
+		System.out.println(hong);
 	}
 }

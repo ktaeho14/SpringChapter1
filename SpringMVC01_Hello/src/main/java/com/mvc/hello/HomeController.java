9c4ecae9102815a4ed435d0147dc0e287156fd93
ktaeho14 <ktaeho14@naver.com>
2021-08-09 19:31:49 +0900

    create SpringMVC수업내용

---
+++ SpringMVC01_Hello/src/main/java/com/mvc/hello/HomeController.java
@@ -0,0 +1,52 @@
+package com.mvc.hello;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mvc.hello.dto.AddressDto;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+		//view 이름
+	}
+	
+	@RequestMapping(value="/command.do", method=RequestMethod.GET)
+	public String getCommand(Model model, String name, String addr, String phone) {
+		System.out.println(name);
+		
+		model.addAttribute("dto", new AddressDto(name, addr, phone));
+		
+		
+		return "getcommand";
+	}
+	
+	
+	
+}
